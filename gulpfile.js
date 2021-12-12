47aa9c93a49072eb0b8a6fc0262dc30ce84a8f72
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-12-12 15:06:20 +0200

    Replace gulp concat with importing

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 const gulp = require('gulp')
-const argv = require('yargs').argv
-const concat = require('gulp-concat')
 const sass = require('gulp-sass')(require('sass'))
 
 gulp.task('default', async cb => {
-    const platform = argv.platform ?? argv.p ?? 'guilded'
     // Compiles all SASS files to CSS
-    gulp.src(['./base.sass', `./src/${platform}/**/*.sass`])
-        .pipe(concat(`${platform}.sass`))
+    gulp.src(`./src/modules/*.sass`)
         .pipe(sass({ indentedSyntax: false, outputStyle: 'compressed' }))
         .pipe(gulp.dest(`./use`))
 })
