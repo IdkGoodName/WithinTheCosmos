0c55591642b863fe10ca6bb3e375fb1a423e58c6
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2022-01-12 21:49:52 +0200

    Prepare for colour schemes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const sass = require("gulp-sass")(require("node-sass"));
+const gulp = require("gulp");
+const path = require("path");
+const fs = require("fs");
 
-gulp.task('default', async cb => {
+gulp.task("default", async cb => {
+    const schemePath = path.join("./src/schemes", "AbsoluteNihility") + ".sass";
+
     // Compiles all SASS files to CSS
     gulp.src(`./src/modules/*.sass`)
-        .pipe(sass({ indentedSyntax: false, outputStyle: 'compressed' }))
-        .pipe(gulp.dest(`./use`))
-})
+        .pipe(
+            sass({
+                indentedSyntax: true,
+                outputStyle: "compressed",
+                importer: (url, prev) => {
+                    console.log("A", url, prev);
+                    return { file: url };
+                }
+            })
+        )
+        .pipe(gulp.dest(`./use`));
+});
